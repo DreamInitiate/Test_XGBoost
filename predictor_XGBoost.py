d9f0e983b1583f589f7430b377e2d852a2ba19c3
DreamInitiate <164319686+DreamInitiate@users.noreply.github.com>
2026-01-05 22:09:49 +0800

    Update predictor_XGBoost.py

--- predictor_XGBoost.py
+++ predictor_XGBoost.py
@@ -143,24 +143,6 @@ plt.tight_layout()
 plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=300)
 st.image("shap_force_plot.png", caption='SHAP Force Plot Explanation')
 
-    # LIME Explanation
-    st.subheader("LIME Explanation")
-    lime_explainer = LimeTabularExplainer(
-        training_data=X_test.values,
-        feature_names=X_test.columns.tolist(),
-        class_names=['Not poor outcome', 'poor outcome'],  # Adjust class names to match your classification task
-        mode='classification'
-    )
     
-    # Explain the instance
-    lime_exp = lime_explainer.explain_instance(
-        data_row=features.flatten(),
-        predict_fn=model.predict_proba
-    )
-
-    # Display the LIME explanation without the feature value table
-    lime_html = lime_exp.as_html(show_table=False)  # Disable feature value table
-
-    st.components.v1.html(lime_html, height=800, scrolling=True)
 
 
